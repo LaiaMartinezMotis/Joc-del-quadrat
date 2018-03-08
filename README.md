7ea387267c9d62a974e76101efe383e734e928f5
Laia <36154539+LaiaMartinezMotis@users.noreply.github.com>
2018-03-09 00:51:21 +0100

    V.06
    # V.06 New version of the Square-Game
     Now it adds:
    -Background, a playanle character and bullets.
    -Pluse it adds music and fx.

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # V.06 New version of the Square-Game
  Now it adds:
--Movement of the rectangle (UP-DOWN-LEFT-RIGHT)
--The oportunity to shoot with the rectangle pressing the SPECE key.
-------
--Diagonal Movement
--New libraries SDL
+-Background, a playanle character and bullets.
+-Pluse it adds music and fx.
--- Square-Game/Square Game/main.cpp
+++ Square-Game/Square Game/main.cpp
@@ -1,67 +1,98 @@
 #include "../SDL/include/SDL.h"
 #include "../SDL_IMAGE/include/SDL_image.h"
+#include "../SDL_Mixer/include/SDL_mixer.h"
 #include <iostream>
+
+
 #pragma comment(lib, "../SDL/libx86/SDL2.lib")
 #pragma comment(lib, "../SDL/libx86/SDL2main.lib")
 #pragma comment (lib, "../SDL_IMAGE/libx86/SDL2_image.lib")
+#pragma comment (lib, "../SDL_Mixer/libx86/SDL2_mixer.lib")
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-SDL_Window*window = NULL; //Create window
-SDL_Renderer*renderer = NULL;//Create render
-SDL_Texture* Texture = NULL;// Create texture 1
+const int SCREEN_WIDTH = 900;
+const int SCREEN_HEIGHT = 600;
+
+
+SDL_Window* window; //Create window
+SDL_Renderer* renderer;//Create render
+SDL_Texture* background;// Create texture 1
+SDL_Texture* character;//Create texture 27
+SDL_Texture* bullet;
+Mix_Music *music;
+Mix_Chunk *gomu;
+//Create texture 3
 
 //Texture Funcion
-	  SDL_Texture* loadTexture(std::string path) { 
+SDL_Texture* loadTexture(std::string path) { 
+
+//Creating the texture
 		SDL_Texture* ATexture=NULL;
-		SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-		ATexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-		SDL_FreeSurface(loadedSurface);
+		//Surface where we load it
+		SDL_Surface* loadSurface = IMG_Load(path.c_str());
+		ATexture = SDL_CreateTextureFromSurface(renderer, loadSurface);
+		SDL_FreeSurface(loadSurface);
 		return ATexture;
 	}
+
+
+
+
 int main(int argc, char*args[]) {
-	
+
+	int aim=0;
+
+	//Inicializing SDL
 	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+	Mix_Init(MIX_INIT_OGG);
+	Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 2048);//frequency(44100 o 22050), Uint format, channels, chunksize
 
 	//Create Window
-	window = SDL_CreateWindow("Create Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	int imgFlags = IMG_INIT_PNG;
-	
-
-	Texture = loadTexture("../Photos/Background.png");
-
-	
-	
+	window = SDL_CreateWindow("Square-Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, 0);
 
 
-	(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"));
-	
-	//Create the items
-	SDL_Rect rectangle; //Creem rectangle
-	rectangle.x = 260;// Posició x en l'espai
-	rectangle.y = 190;// Posició y en l'espai
-	rectangle.w = 120;// Amplada
-	rectangle.h = 80;// ALt
-
-	SDL_Rect rectangle1; //Creem el segon rectangle
-	rectangle1.x = NULL;
-	rectangle1.y = NULL;
-	rectangle1.w = NULL;
-	rectangle1.h = NULL; 
 
 
-		//Inicialize the render
-	
+	//Player Surface
+	SDL_Rect* Player_Surface = new SDL_Rect;
+	Player_Surface->x = 0;
+	Player_Surface->y = 0;
+	Player_Surface->w = 120;
+	Player_Surface->h = 80;
 
-//Bucle
+
+
+	//Bullet Surface
+	SDL_Rect* Bull_Surface[5];
+	for (int c = 0; c < 5; c++)
+	{
+		Bull_Surface[c] = new SDL_Rect;
+		Bull_Surface[c]->x;
+		Bull_Surface[c]->y;
+		Bull_Surface[c]->w=40;
+		Bull_Surface[c]->h=40;
+	}
+	//Textures
+	background = loadTexture("Background.png");
+	character = loadTexture("luffy.png");
+	bullet = loadTexture("arm.png");
+
+	music = Mix_LoadMUS("music.ogg");
+	gomu = Mix_LoadWAV("arm.wav");
+
+
+	Mix_PlayMusic(music, 1);
+	Mix_VolumeMusic(45);
+	//Bucle
+	// Inicialize the loop
 	bool loop = true;
 	bool moveup = false;
 	bool movedown = false;
 	bool moveright = false;
 	bool moveleft = false;
-	// Inicialize the loop
-	bool Bala = false;// Create an afirmation
+
+	bool shooting = false;
 	while (loop)
 	{
 		SDL_Event event; // Inputs
@@ -71,15 +102,19 @@ int main(int argc, char*args[]) {
 				loop = false;
 			}
 
-			if(event.type==SDL_KEYDOWN){   //Moviment i atac
-				if (event.key.keysym.sym== SDLK_UP) {// Move up the red rec.
+			if (event.type == SDL_KEYDOWN) {   //Moviment i atac
+
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+					loop = false;
+				}
+
+				if (event.key.keysym.sym == SDLK_UP) {// Move up the red rec.
 					moveup = true;
-					}
-					
+				}
 				if (event.key.keysym.sym == SDLK_DOWN) {// Move down the red rec.
 					movedown = true;
 				}
-					
+
 				if (event.key.keysym.sym == SDLK_RIGHT) {// Move right the red rec.
 					moveright = true;
 				}
@@ -88,15 +123,11 @@ int main(int argc, char*args[]) {
 				}
 
 				if (event.key.keysym.sym == SDLK_SPACE) {//Create the bullet
+					shooting = true;
 
-					Bala = true;//If we presss space then the bullet appears
-					rectangle1.x = (rectangle.x) + 120;
-					rectangle1.y = (rectangle.y) + 25;
-					rectangle1.h = 20;
-					rectangle1.w = 30;
 				}
 			}
-			if (event.type == SDL_KEYUP) {   //Moviment i atac
+			if (event.type == SDL_KEYUP) {   //Moviment and atac
 				if (event.key.keysym.sym == SDLK_UP) {// Move up the red rec.
 					moveup = false;
 				}
@@ -111,80 +142,77 @@ int main(int argc, char*args[]) {
 				if (event.key.keysym.sym == SDLK_LEFT) {// Move left the red rec.
 					moveleft = false;
 				}
+				if (event.key.keysym.sym == SDLK_SPACE) {// Move left the red rec.
+
+					 shooting = false;
+				}
 			}
-			
+
 		}
 
-//Borders in the screen
-		if (rectangle.y < 0) {
-			rectangle.y = 0;
+		//Borders in the screen
+		if (Player_Surface->x < 0) {
+			Player_Surface->x = 0;
 		}
-		if (rectangle.y > 400) {
-			rectangle.y = 400;
+		if (Player_Surface->x > SCREEN_WIDTH - Player_Surface->w) {
+			Player_Surface->x = SCREEN_WIDTH - Player_Surface->w;
 		}
-		if (rectangle.x < 0) {
-			rectangle.x = 0 ;
+		if (Player_Surface->y < 0) {
+			Player_Surface->y = 0;
 		}
-		if (rectangle.x > 520) {
-			rectangle.x = 520;
+		if (Player_Surface->y > SCREEN_HEIGHT - Player_Surface->h) {
+			Player_Surface->y = SCREEN_HEIGHT - Player_Surface->h;
 		}
+
+		SDL_RenderCopy(renderer, background, 0, 0);
+		SDL_RenderCopy(renderer, character, 0, Player_Surface);
+		
+
+
+		//Texture Bullet
+		for (int c = 0; c < 5; c++) {
+			SDL_RenderCopy(renderer, bullet, 0, Bull_Surface[c]);
+			Bull_Surface[c]->x += 5;
+		}
+
+		//Shooting
+		if (shooting) {
+			Bull_Surface[aim]->x = (Player_Surface->x) + (Player_Surface->w);
+			Bull_Surface[aim]->y = (Player_Surface->y) + ((Player_Surface->h) / 2 - Bull_Surface[1]->h);
+			Mix_PlayChannel(-1, gomu, 0);
+			aim++;
+			if (aim == 5) {
+				aim = 0;
+			}
+			shooting = false;
+		}
+
 		//Movement
-		if (moveup == true){
-				rectangle.y -= 15;
-		}
-		if (movedown == true) {
-			rectangle.y += 15;
-		}
+
 		if (moveleft == true) {
-				rectangle.x -= 15;
+			Player_Surface->x -= 15;
 		}
 		if (moveright == true) {
-			rectangle.x += 15;
+			Player_Surface->x += 15;
 		}
-
-	SDL_RenderCopy(renderer, Texture, 0, 0);
-
-		//Render  rojo
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);// Color quadrat gran
-		SDL_RenderFillRect(renderer, &rectangle);// Rellenem el quadrat amb el color
-
-												
-	    //Renderizar Cuadrado verde
-		if (Bala = true) {
-			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-			SDL_RenderFillRect(renderer, &rectangle1);
-			rectangle1.x += 10;
-			
+		if (moveup == true) {
+			Player_Surface->y -= 15;
 		}
-		
-		//Renderitzar finerstra
-
-		
-		SDL_RenderClear(renderer);//Pinta la pantalla de color
-								  /*if (rectangle.x <= 520) {
-								  rectangle.x++;
-								  if (rectangle.x == 520) {
-								  rectangle.x = 0;
-								  }
-								  }*/
-								  //Render Textura
+		if (movedown == true) {
+			Player_Surface->y += 15;
+		}
 	
+		SDL_RenderPresent(renderer);
 		SDL_Delay(18);
-		SDL_DestroyTexture(Texture);
-		Texture = NULL;
 	}
-	SDL_DestroyTexture(Texture);
-    Texture = NULL;
-
-	//Destroy window    
+	Mix_FreeMusic(music);
+	Mix_FreeChunk(gomu);
+	SDL_DestroyTexture(background);
+	SDL_DestroyTexture(bullet);
+	SDL_DestroyTexture(character);
 	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	window = NULL;
-	renderer = NULL;
-
-	//Quit SDL subsystems
+	SDL_DestroyWindow(window);	
 	IMG_Quit();
 	SDL_Quit();
-
 	return 0;
 }
